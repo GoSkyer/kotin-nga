b32708eb846a54f9adf3e33c8855e78e54ac30ad
guozhong@9188.com <guozhong@9188.com>
2017-06-15 19:22:42 +0800

    重新调整架构

--- BaseProject/app/src/main/java/com/huangbo/baseprojecet/modle/MainModle.java
+++
@@ -1,9 +0,0 @@
-package com.huangbo.baseprojecet.modle;
-
-/**
- * Created by huangb on 2017/5/11.
- */
-
-public interface MainModle {
-    void getData(String ip);
-}
--- BaseProject/app/src/main/java/com/huangbo/baseprojecet/modle/TestModle.java
+++
@@ -1,9 +0,0 @@
-package com.huangbo.baseprojecet.modle;
-
-/**
- * Created by huangb on 2017/5/11.
- */
-
-public interface TestModle {
-    void getIpInfo(String ip);
-}
