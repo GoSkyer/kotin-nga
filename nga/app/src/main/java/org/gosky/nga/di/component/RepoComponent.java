c75d179e0f1517024e60872257f15234ef4d9872
zohar-soul <102589617@qq.com>
2017-06-25 20:18:29 +0800

    #refactor 删除无用文件和资源

--- nga/app/src/main/java/org/gosky/nga/di/component/RepoComponent.java
+++ nga/app/src/main/java/org/gosky/nga/di/component/RepoComponent.java
@@ -3,11 +3,7 @@ package org.gosky.nga.di.component;
 import org.gosky.base.di.scope.ActivityScope;
 import org.gosky.nga.ui.activity.MainActivity;
 import org.gosky.nga.ui.activity.ForumActivity;
-import org.gosky.nga.ui.fragment.HomeFragment;
-import org.gosky.nga.ui.fragment.LoanFragment;
 import org.gosky.nga.ui.fragment.MainFragment;
-import org.gosky.nga.ui.fragment.MineFragment;
-import org.gosky.nga.ui.fragment.RecommendFragment;
 
 import dagger.Component;
 
@@ -16,14 +12,6 @@ import dagger.Component;
 @Component(dependencies = {AppComponent.class, ActivityComponent.class})
 public interface RepoComponent {
     void inject(MainActivity activity);
-
-    void inject(HomeFragment homeFragment);
-
-    void inject(LoanFragment loanFragment);
-
-    void inject(RecommendFragment recommendFragment);
-
-    void inject(MineFragment mineFragment);
 
     void inject(MainFragment mainFragment);
 
